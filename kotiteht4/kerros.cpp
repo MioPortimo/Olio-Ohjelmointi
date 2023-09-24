259dea9478541e3fd3cf54db1bd6d81af55c48df
MioPortimo <143484280+MioPortimo@users.noreply.github.com>
2023-09-24 17:44:00 +0300

    Update kerros.cpp

--- kotiteht4/kerros.cpp
+++ kotiteht4/kerros.cpp
@@ -10,9 +10,6 @@ void Kerros::maaritaAsunnot()
     cout<<"Maaritetaan katutason kerrokselta perittyja asuntoja"<<endl;
     cout<<"Maaritetaan 4 kpl kerroksen asuntoja"<<endl;
     cout<<"Asunto maaritety asukkaita= "<<asukasMaara<<" nelioita= "<<neliot<<endl;
-    cout<<"Asunto maaritety asukkaita= "<<asukasMaara<<" nelioita= "<<neliot<<endl;
-    cout<<"Asunto maaritety asukkaita= "<<asukasMaara<<" nelioita= "<<neliot<<endl;
-    cout<<"Asunto maaritety asukkaita= "<<asukasMaara<<" nelioita= "<<neliot<<endl;
 }
 double Kerros::laskeKulutus(double h)
 {
