fe65bf760fa40596aca50abb0fa188ada4ec5ec6
MioPortimo <143484280+MioPortimo@users.noreply.github.com>
2023-09-25 09:59:55 +0300

    Update kerros.h

--- kotiteht4/kerros.h
+++ kotiteht4/kerros.h
@@ -5,13 +5,14 @@
 using namespace std;
 
 
-class Kerros:public Asunto
+class Kerros
 {
 public:
     Kerros();
     Asunto as1,as2,as3,as4;
-    virtual void maaritaAsunnot();
-    virtual double laskeKulutus(double);
+    virtual void maaritaAsunnot(void);
+    double laskeKulutus(double);
 };
 
 #endif // KERROS_H
+
