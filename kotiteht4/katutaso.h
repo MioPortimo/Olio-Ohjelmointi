cc5762fc51b50b222543ca5d8e42e006c765b655
MioPortimo <143484280+MioPortimo@users.noreply.github.com>
2023-09-25 09:58:38 +0300

    Update katutaso.h

--- kotiteht4/katutaso.h
+++ kotiteht4/katutaso.h
@@ -1,16 +1,16 @@
 #ifndef KATUTASO_H
 #define KATUTASO_H
-#include "asunto.h"
+#include "kerros.h"
 #include <iostream>
 using namespace std;
 
-class Katutaso:public Asunto
+class Katutaso:public Kerros
 {
 public:
     Katutaso();
     Asunto as1,as2;
-    virtual void maaritaAsunnot();
-    virtual double laskeKulutus(double);
+    virtual void maaritaAsunnot(void)override;
+    double laskeKulutus(double);
 };
 
 #endif // KATUTASO_H
