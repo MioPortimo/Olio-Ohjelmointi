bd01373cdf67b5065bb30e5eea255df2ddb99927
MioPortimo <143484280+MioPortimo@users.noreply.github.com>
2023-10-01 20:54:02 +0300

    Update kerros.cpp

--- Kotiteht5/kerros.cpp
+++ Kotiteht5/kerros.cpp
@@ -3,18 +3,32 @@
 Kerros::Kerros()
 {
     cout<<"Kerros luotu"<<endl;
+    pas1 = new Asunto;
+    pas2 = new Asunto;
+    pas3 = new Asunto;
+    pas4 = new Asunto;
 
 }
 void Kerros::maaritaAsunnot()
 {
     cout<<"Maaritetaan 4 kpl kerroksen asuntoja"<<endl;
-    as1->maarita(2,100);
-    as2->maarita(2,100);
-    as3->maarita(2,100);
-    as4->maarita(2,100);
+    pas1.maarita(2,100);
+    pas2.maarita(2,100);
+    pas3.maarita(2,100);
+    pas4.maarita(2,100);
 }
 double Kerros::laskeKulutus(double h)
 {
-
-    return as1->laskeKulutus(h)+as2->laskeKulutus(h)+as3->laskeKulutus(h)+as4->laskeKulutus(h);
+    return pas1.laskeKulutus(h)+pas2.laskeKulutus(h)+pas3.laskeKulutus(h)+pas4.laskeKulutus(h);
+}
+Kerros::~Kerros()
+{
+    delete pas1;
+    pas1 = nullptr;
+    delete pas2;
+    pas2 = nullptr;
+    delete pas3;
+    pas3 = nullptr;
+    delete pas4;
+    pas4 = nullptr;
 }
