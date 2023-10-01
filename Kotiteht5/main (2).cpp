0b9939c17246713cd54238c48ab1abb3dfab03b8
MioPortimo <143484280+MioPortimo@users.noreply.github.com>
2023-10-01 21:02:59 +0300

    Update main (2).cpp

--- Kotiteht5/main (2).cpp
+++ Kotiteht5/main (2).cpp
@@ -3,14 +3,11 @@
 
 int main()
 {
-    Asunto olio1;
-    Katutaso olio2;
-    Kerrostalo olio3;
-    olio1.maarita(2,100);
-    cout<<"Asunnon kulutus, kun hinta = 1 on "<<olio1.laskeKulutus(1)<<endl;
-    olio2.maaritaAsunnot();
-    cout<<"Katutason ja perityn kerroksen kulutus, kun hinta = 1 on"<<olio2.laskeKulutus(1)<<endl;
-    cout<<"Kerrostalon kulutus, ="<<olio3.laskeKulutus(1)<<endl;
+    Kerrostalo* pkerrostalo = new kerrostalo;
+    pkerrostalo->maaritaAsunnot();
+    cout<<"Kerrostalon kulutus: "<<pkerrostalo.laskeKulutus(1)<<endl;
+    delete pkerrostalo;
+    pkerrostalo = nullptr;
 
     return 0;
 }
