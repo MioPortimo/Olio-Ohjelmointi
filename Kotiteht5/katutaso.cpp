7fa52d5715ff277640223b427670309944ec859e
MioPortimo <143484280+MioPortimo@users.noreply.github.com>
2023-10-01 20:49:39 +0300

    Update katutaso.cpp

--- Kotiteht5/katutaso.cpp
+++ Kotiteht5/katutaso.cpp
@@ -4,18 +4,25 @@
 Katutaso::Katutaso()
 {
     cout<<"Katutaso luotu"<<endl;
+    pas1 = new Asunto;
+    pas2 = new Asunto;
 
 }
 void Katutaso::maaritaAsunnot()
 {
 
     cout<<"Maaritetaan 2 kpl katutason asuntoja"<<endl;
-    as1->maarita(2,100);
-    as2->maarita(2,100);
+    pas1.maarita(2,100);
+    pas2.maarita(2,100);
     cout<<"Maaritetaan katutason kerrokselta perittyja asuntoja"<<endl;
     this->Kerros::maaritaAsunnot();
 }
 double Katutaso::laskeKulutus(double h)
 {
-    return as1->laskeKulutus(h)+as2->laskeKulutus(h)+Kerros::laskeKulutus(h);
+    return pas1.laskeKulutus(h)+pas2.laskeKulutus(h)+Kerros::laskeKulutus(h);
+}
+Katutaso::~Katutaso()
+{
+    delete pas1;
+    delete pas2;
 }
