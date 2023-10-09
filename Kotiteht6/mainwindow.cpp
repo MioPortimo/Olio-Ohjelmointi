da75b14f4d81e0b31adf5b9ef78525e33554a55a
MioPortimo <143484280+MioPortimo@users.noreply.github.com>
2023-10-09 13:53:31 +0300

    Update mainwindow.cpp

--- Kotiteht6/mainwindow.cpp
+++ Kotiteht6/mainwindow.cpp
@@ -6,24 +6,10 @@ MainWindow::MainWindow(QWidget *parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
-    result = 0.00;
     state = 1;
-    connect(ui->Clear,SIGNAL(clicked(bool)),this,SLOT(clearAndEnterClickedHandler()));
-    connect(ui->Enter,SIGNAL(clicked(bool)),this,SLOT(clearAndEnterClickedHandler()));
-    connect(ui->Jako,SIGNAL(clicked(bool)),this,SLOT(addSubMulDivClickedHandler()));
-    connect(ui->Plus,SIGNAL(clicked(bool)),this,SLOT(addSubMulDivClickedHandler()));
-    connect(ui->Minus,SIGNAL(clicked(bool)),this,SLOT(addSubMulDivClickedHandler()));
-    connect(ui->Kerto,SIGNAL(clicked(bool)),this,SLOT(addSubMulDivClickedHandler()));
-    connect(ui->N0,SIGNAL(clicked(bool)),this,SLOT(numberClickedHandler()));
-    connect(ui->N1,SIGNAL(clicked(bool)),this,SLOT(numberClickedHandler()));
-    connect(ui->N2,SIGNAL(clicked(bool)),this,SLOT(numberClickedHandler()));
-    connect(ui->N3,SIGNAL(clicked(bool)),this,SLOT(numberClickedHandler()));
-    connect(ui->N4,SIGNAL(clicked(bool)),this,SLOT(numberClickedHandler()));
-    connect(ui->N5,SIGNAL(clicked(bool)),this,SLOT(numberClickedHandler()));
-    connect(ui->N6,SIGNAL(clicked(bool)),this,SLOT(numberClickedHandler()));
-    connect(ui->N7,SIGNAL(clicked(bool)),this,SLOT(numberClickedHandler()));
-    connect(ui->N8,SIGNAL(clicked(bool)),this,SLOT(numberClickedHandler()));
-    connect(ui->N9,SIGNAL(clicked(bool)),this,SLOT(numberClickedHandler()));
+    number1 = "";
+    number2 = "";
+    resetLineEdits();
 }
 
 MainWindow::~MainWindow()
@@ -31,25 +17,206 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
+// Number clicked handlers....
+
 void MainWindow::numberClickedHandler()
+{
+
+    QPushButton * button = qobject_cast<QPushButton*>(sender());
+    QString name = button->objectName();
+    qDebug() << "Button name:" << name;
+
+
+    if(state == 1) // user is giving first number
+    {
+        number1 += name.last(1);
+        ui->Num1->setText(number1);
+    }
+    else          // user is giving second number
+    {
+        number2 += name.last(1);
+        ui->Num2->setText(number2);
+    }
+    qDebug()<<"number 1 = "<< number1<<Qt::endl;
+    qDebug()<<"number 2 = "<< number2<<Qt::endl;
+}
+
+void MainWindow::clearAndEnterClickHandler()
 {
     QPushButton * button = qobject_cast<QPushButton*>(sender());
     QString name = button->objectName();
-    qDebug()<<"Button name: "<<name;
+    qDebug() << "Button name:" << name;
+    if(name == "clear")
+    {
+        state = 1;
+        number1 = "";
+        number2 = "";
+        resetLineEdits();
+    }
+    else // if(name == enter)
+    {
+        float n1 = number1.toFloat();
+        float n2 = number2.toFloat();
+        qDebug()<<"number 1 = "<< n1 << " and number 2 = "<<n2 <<Qt::endl;
+
+        switch(operand) {
+          case 0:
+            result = n1+n2;
+            break;
+          case 1:
+            result = n1-n2;
+            break;
+          case 2:
+            result = n1*n2;
+            break;
+          case 3:
+            result = n1/n2;
+            break;
+
+          default:
+            qDebug()<<"Not a correct operand"<<Qt::endl;
+        }
+        ui->Result->setText(QString::number(result));
+    }
+
 }
 
-void MainWindow::clearAndEnterClickedHandler()
+void MainWindow::addSubMulDivClickHandler()
 {
-
-}
-
-void MainWindow::addSubMulDivClickedHandler()
-{
+    QPushButton * button = qobject_cast<QPushButton*>(sender());
+    QString name = button->objectName();
+    qDebug() << "Button name:" << name;
+    if(name == "add")
+    {
+        operand = 0;
+    }
+    else if(name == "sub")
+    {
+        operand = 1;
+    }
+    else if(name == "mul")
+    {
+        operand = 2;
+    }
+    else if(name == "div")
+    {
+        operand = 3;
+    }
+    state = 2;
+    qDebug()<<"operand = "<< operand << Qt::endl;
 
 }
 
 void MainWindow::resetLineEdits()
 {
-
+    ui->Num1->clear();
+    ui->Num2->clear();
+    ui->Result->clear();
 }
+
+
+
+void MainWindow::on_N1_clicked()
+{
+    numberClickedHandler();
+}
+
+
+void MainWindow::on_N2_clicked()
+{
+    numberClickedHandler();
+}
+
+
+void MainWindow::on_N3_clicked()
+{
+    numberClickedHandler();
+}
+
+
+void MainWindow::on_N4_clicked()
+{
+    numberClickedHandler();
+}
+
+
+void MainWindow::on_N5_clicked()
+{
+    numberClickedHandler();
+}
+
+
+void MainWindow::on_N6_clicked()
+{
+    numberClickedHandler();
+}
+
+
+void MainWindow::on_N7_clicked()
+{
+    numberClickedHandler();
+}
+
+
+void MainWindow::on_N8_clicked()
+{
+    numberClickedHandler();
+}
+
+
+void MainWindow::on_N9_clicked()
+{
+    numberClickedHandler();
+}
+
+
+void MainWindow::on_N0_clicked()
+{
+    numberClickedHandler();
+}
+
+// add, sub, mul, div handlers...
+
+
+
+
+
+
+void MainWindow::on_add_clicked()
+{
+    addSubMulDivClickHandler();
+}
+
+
+void MainWindow::on_sub_clicked()
+{
+    addSubMulDivClickHandler();
+}
+
+
+void MainWindow::on_mul_clicked()
+{
+    addSubMulDivClickHandler();
+}
+
+
+void MainWindow::on_div_clicked()
+{
+    addSubMulDivClickHandler();
+}
+
+
+// clear and enter handlers
+
+void MainWindow::on_clear_clicked()
+{
+    clearAndEnterClickHandler();
+}
+
+
+void MainWindow::on_enter_clicked()
+{
+    clearAndEnterClickHandler();
+}
+
 
